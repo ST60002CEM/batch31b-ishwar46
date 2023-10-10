1602809b61fb3626562375e0d02a82c1c7426007
Ishwar Chaudhary <ishwarura@gmail.com>
2023-10-10 16:50:16 +0545

    Update login_page.dart

--- lib/views/login/login_page.dart
+++ lib/views/login/login_page.dart
@@ -1,1 +1,68 @@
+import 'package:flutter/material.dart';
 
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(children: [
+        Container(
+          height: 300,
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/img/logo.png"),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        const Text(
+          "Welcome",
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: "Enter Username",
+                  labelText: "Username",
+                ),
+              ),
+              TextFormField(
+                obscureText: true,
+                decoration: const InputDecoration(
+                  hintText: "Enter Password",
+                  labelText: "Password",
+                ),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  //Navigator.pushNamed(context, MyRoutes.homeRoute);
+                },
+                child: const Text("Login"),
+                style: TextButton.styleFrom(minimumSize: const Size(150, 40)),
+              ),
+            ],
+          ),
+        ),
+      ]),
+    );
+  }
+}
