0f402210840e8a32ef58d2b4af1435ed820d6c0e
Ishwar Chaudhary <ishwarura@gmail.com>
2023-10-10 16:49:47 +0545

    App Color Added

---
+++ lib/styles/appcolor.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+mixin AppColor {
+  static const Color primaryColor = Color(0xFF0F4B7D);
+  static const Color accent = Color.fromARGB(255, 29, 118, 191);
+  static const Color secondaryColor = Color.fromARGB(255, 167, 19, 76);
+}
