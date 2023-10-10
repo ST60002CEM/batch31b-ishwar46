935dd859a7a52a0426ebf0e344d47d8c6ddddae0
Ishwar Chaudhary <ishwarura@gmail.com>
2023-10-10 16:31:20 +0545

    Minor Update

--- lib/views/home/home_page.dart
+++ lib/views/home/home_page.dart
@@ -10,6 +10,24 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 15, 75, 125),
+          title: const Text("DASHBOARD"),
+          elevation: 0,
+          leading: IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () {},
+          ),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.notifications),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        body: Center(
+          child: Image.asset("assets/img/logo.png"),
+        ));
   }
 }
