bf954e97c79b5f6564fc9aca119b5995ee36fde8
Ishwar Chaudhary <ishwarura@gmail.com>
2023-10-11 15:53:07 +0545

    Login Page Update and Localization Package Added

--- lib/views/login/login_page.dart
+++ lib/views/login/login_page.dart
@@ -1,4 +1,7 @@
+import 'package:age_care/widgets/background_widget.dart';
 import 'package:flutter/material.dart';
+
+import '../../widgets/login_button_widget.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -8,61 +11,94 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  bool _passwordVisiable = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-        Container(
-          height: 300,
-          width: double.infinity,
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("assets/img/logo.png"),
-              fit: BoxFit.cover,
+      body: Container(
+        child: Column(children: [
+          const SizedBox(
+            height: 300,
+            width: double.infinity,
+            child: BackGroundWidget(),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text(
+            "LOGIN",
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
             ),
           ),
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        const Text(
-          "Welcome",
-          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-          child: Column(
-            children: [
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: "Enter Username",
-                  labelText: "Username",
-                ),
-              ),
-              TextFormField(
-                obscureText: true,
-                decoration: const InputDecoration(
-                  hintText: "Enter Password",
-                  labelText: "Password",
-                ),
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  //Navigator.pushNamed(context, MyRoutes.homeRoute);
-                },
-                child: const Text("Login"),
-                style: TextButton.styleFrom(minimumSize: const Size(150, 40)),
-              ),
-            ],
+          const SizedBox(
+            height: 20,
           ),
-        ),
-      ]),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 16,
+              horizontal: 32,
+            ),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: "Enter Username",
+                    labelText: "Username",
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(
+                      Icons.person,
+                      size: 20,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                TextFormField(
+                  obscureText: !_passwordVisiable,
+                  decoration: const InputDecoration(
+                    hintText: "Enter Password",
+                    labelText: "Password",
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(
+                      Icons.lock,
+                      size: 20,
+                    ),
+                    suffixIcon: IconButton(
+                      onPressed: null,
+                      icon: Icon(Icons.visibility),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Password cannot be empty";
+                    } else if (value.length < 6) {
+                      return "Password length should be atleast 6";
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                LoginButton(
+                  text: "Login",
+                  onTap: () {
+                    Navigator.pushNamed(context, '/home');
+                  },
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const TextButton(
+                  onPressed: null,
+                  child: Text("Herchaha Mobile Application Version 1.0.0"),
+                )
+              ],
+            ),
+          ),
+        ]),
+      ),
     );
   }
 }
---
+++ lib/widgets/background_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class BackGroundWidget extends StatelessWidget {
+  const BackGroundWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 300,
+      width: double.infinity,
+      child: Placeholder(),
+    );
+  }
+}
