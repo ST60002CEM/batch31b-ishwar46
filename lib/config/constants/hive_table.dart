ab2415a5e169914a1a283a1f661d31e3b3c37fa8
Ishwar Chaudhary <ishwarura@gmail.com>
2023-12-29 17:03:30 +0545

    Create hive_table.dart

---
+++ lib/config/constants/hive_table.dart
@@ -0,0 +1,24 @@
+class HiveTableConstant {
+  HiveTableConstant._();
+
+  static const int staffTableId = 0;
+  static const String staffTable = 'staffBox';
+
+  static const int clientTableId = 1;
+  static const String clientTable = 'clientBox';
+
+  static const int appointmentTableId = 2;
+  static const String appointmentTable = 'appointmentBox';
+
+  static const int serviceTableId = 3;
+  static const String serviceTable = 'serviceBox';
+
+  static const int productTableId = 4;
+  static const String productTable = 'productBox';
+
+  static const int invoiceTableId = 5;
+  static const String invoiceTable = 'invoiceBox';
+
+  static const int paymentTableId = 6;
+  static const String paymentTable = 'paymentBox';
+}
