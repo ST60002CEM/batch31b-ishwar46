2abcd3e0ceb118e4ec9da7d5c9367c3f469dbba7
Ishwar Chaudhary <ishwarura@gmail.com>
2024-01-06 21:02:47 +0545

    Update auth_remote_data_source.dart

--- lib/features/auth/data/data_source/auth_remote_data_source.dart
+++ lib/features/auth/data/data_source/auth_remote_data_source.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -22,39 +20,39 @@ class AuthRemoteDataSource {
   AuthRemoteDataSource(this.dio);
 
   // Upload image using multipart
-  Future<Either<Failure, String>> uploadProfilePicture(
-    File image,
-  ) async {
+  // Future<Either<Failure, String>> uploadProfilePicture(
+  //   File image,
+  // ) async {
+  //   try {
+  //     String fileName = image.path.split('/').last;
+  //     FormData formData = FormData.fromMap(
+  //       {
+  //         'profilePicture': await MultipartFile.fromFile(
+  //           image.path,
+  //           filename: fileName,
+  //         ),
+  //       },
+  //     );
+
+  //     Response response = await dio.post(
+  //       ApiEndpoints.uploadImage,
+  //       data: formData,
+  //     );
+
+  //     return Right(response.data["data"]);
+  //   } on DioException catch (e) {
+  //     return Left(
+  //       Failure(
+  //         error: e.error.toString(),
+  //         statusCode: e.response?.statusCode.toString() ?? '0',
+  //       ),
+  //     );
+  //   }
+  // }
+
+  Future<Either<Failure, bool>> registerStaff(AuthEntity staff) async {
     try {
-      String fileName = image.path.split('/').last;
-      FormData formData = FormData.fromMap(
-        {
-          'profilePicture': await MultipartFile.fromFile(
-            image.path,
-            filename: fileName,
-          ),
-        },
-      );
-
-      Response response = await dio.post(
-        ApiEndpoints.uploadImage,
-        data: formData,
-      );
-
-      return Right(response.data["data"]);
-    } on DioException catch (e) {
-      return Left(
-        Failure(
-          error: e.error.toString(),
-          statusCode: e.response?.statusCode.toString() ?? '0',
-        ),
-      );
-    }
-  }
-
-  Future<Either<Failure, bool>> registerStudent(AuthEntity student) async {
-    try {
-      AuthApiModel apiModel = AuthApiModel.fromEntity(student);
+      AuthApiModel apiModel = AuthApiModel.fromEntity(staff);
       Response response = await dio.post(
         ApiEndpoints.register,
         data: {
