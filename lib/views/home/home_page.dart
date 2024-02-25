1f87cc4616dadb4188b261a9b79255a2744e963c
Ishwar Chaudhary <ishwarura@gmail.com>
2024-02-25 22:27:15 +0545

    Refactoring Home Page

--- lib/views/home/home_page.dart
+++ lib/views/home/home_page.dart
@@ -1,26 +1,20 @@
 import 'dart:async';
-
-import 'package:age_care/config/router/app_routes.dart';
-import 'package:age_care/core/utils/helpers/helper_functions.dart';
-import 'package:age_care/views/home/widgets/service_text.dart';
-import 'package:age_care/widgets/list_tile_widget.dart';
-import 'package:awesome_dialog/awesome_dialog.dart';
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:shake/shake.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:iconsax/iconsax.dart';
-
-import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
-import 'package:shake/shake.dart';
-import '../../config/constants/app_colors.dart';
-
-import '../../features/auth/presentation/view/login/login_view.dart';
-import '../../widgets/card_widget.dart';
-import '../../widgets/drawer_widget.dart';
-import 'widgets/service_row.dart';
-import 'widgets/service_row_two.dart';
+import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:age_care/config/router/app_routes.dart';
+import 'package:age_care/core/utils/helpers/helper_functions.dart';
+import 'package:age_care/widgets/drawer_widget.dart';
+import 'package:age_care/config/constants/app_colors.dart';
+import 'package:age_care/views/home/widgets/service_text.dart';
+import 'package:age_care/views/home/widgets/service_row.dart';
+import 'package:age_care/views/home/widgets/service_row_two.dart';
+import 'widgets/emergency_bottom_sheet.dart';
+import 'widgets/slider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -30,54 +24,41 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _currentIndex = 0;
   late ShakeDetector shakeDetector;
   int shakeCount = 0;
   final FlutterSecureStorage secureStorage = FlutterSecureStorage();
-  late String? username;
-
-  // late final Map<String, dynamic> userData;
-  //bool isTokenExpired = _isTokenExpired(); // Check if the token is expired
+  String? username;
 
   @override
   void initState() {
     super.initState();
-    getUsername();
-
-    // Initialize shake detector
     shakeDetector = ShakeDetector.autoStart(
-      onPhoneShake: () {
-        shakeCount++;
-
-        // If the user shakes the phone four times, show the emergency bottom sheet
-        if (shakeCount >= 2) {
-          shakeCount = 0;
-          _showEmergencyBottomSheet();
-        }
-        Timer(Duration(seconds: 3), () {
-          shakeCount = 0;
-        });
-      },
+      onPhoneShake: _handleShake,
     );
-  }
-
-  Future<void> getUsername() async {
-    final username = await HelperFunctions.getUsernameFromToken();
-    setState(() {
-      this.username = username ?? 'Guest';
-    });
+    _getUsername();
   }
 
   @override
   void dispose() {
-    // Dispose of shake detector when the widget is disposed
     shakeDetector.stopListening();
     super.dispose();
   }
 
-  void _onItemTapped(int index) {
+  void _handleShake() {
+    shakeCount++;
+    if (shakeCount >= 2) {
+      shakeCount = 0;
+      _showEmergencyBottomSheet();
+    }
+    Timer(Duration(seconds: 3), () {
+      shakeCount = 0;
+    });
+  }
+
+  Future<void> _getUsername() async {
+    final username = await HelperFunctions.getUsernameFromToken();
     setState(() {
-      _currentIndex = index;
+      this.username = username ?? 'Guest';
     });
   }
 
@@ -85,71 +66,7 @@ class _HomePageState extends State<HomePage> {
     showModalBottomSheet(
       context: context,
       isScrollControlled: true,
-      builder: (BuildContext context) {
-        return SingleChildScrollView(
-          child: Container(
-            padding: EdgeInsets.only(
-              bottom: MediaQuery.of(context).viewInsets.bottom,
-              left: 16,
-              right: 16,
-              top: 16,
-            ),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Report Emergency',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  'Urgent Emergency Call',
-                  style: TextStyle(
-                    color: Colors.red,
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(height: 16),
-                TextField(
-                  maxLines: 4,
-                  decoration: InputDecoration(
-                    hintText: 'Enter emergency details...',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 16),
-                Hero(
-                  tag: 'emergency',
-                  child: SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                      onPressed: () async {
-                        EasyLoading.showSuccess(
-                            'Emergency reported successfully.',
-                            dismissOnTap: false);
-
-                        Navigator.of(context).pop();
-                      },
-                      child: Text('Submit'),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 16),
-              ],
-            ),
-          ),
-        );
-      },
+      builder: (_) => EmergencyBottomSheet(),
     );
   }
 
@@ -166,16 +83,12 @@ class _HomePageState extends State<HomePage> {
       ),
       desc: "Are you sure you want to logout?",
       btnCancelOnPress: () {},
-      btnOkOnPress: () {
-        logout();
-      },
+      btnOkOnPress: logout,
     ).show();
   }
 
-  //Logout and Delete Token
   Future<void> logout() async {
     await secureStorage.delete(key: "authToken");
-
     Navigator.pushReplacementNamed(context, MyRoutes.loginRoute);
   }
 
@@ -186,90 +99,66 @@ class _HomePageState extends State<HomePage> {
       onPopInvoked: (_) async {},
       child: Scaffold(
         backgroundColor: Colors.white.withOpacity(.94),
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 15, 75, 125),
-          title: Text('Welcome! $username'),
-          titleTextStyle: GoogleFonts.montserrat(
-            color: AppColors.whiteText,
-            fontSize: 15,
-            fontWeight: FontWeight.w600,
-          ),
-          elevation: 0,
-          leading: Builder(
-            builder: (BuildContext context) {
-              return IconButton(
-                icon: const Icon(Iconsax.menu, color: AppColors.whiteText),
-                onPressed: () {
-                  Scaffold.of(context).openDrawer();
-                },
-              );
-            },
-          ),
-          actions: [
-            IconButton(
-              color: AppColors.whiteText,
-              icon: const Icon(Icons.notifications),
-              onPressed: () {
-                EasyLoading.showInfo("No new notifications.");
-              },
-            ),
-            IconButton(
-              color: AppColors.whiteText,
-              icon: const Icon(Icons.logout),
-              onPressed: () {
-                _showLogoutDialog();
-              },
-            ),
-          ],
-        ),
+        appBar: _buildAppBar(),
         drawer: MyDrawer(),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: Column(
-              children: [
-                CarouselSlider(
-                  options: CarouselOptions(
-                    clipBehavior: Clip.antiAlias,
-                    aspectRatio: 10.0,
-                    height: 120.0,
-                    autoPlay: true,
-                    autoPlayInterval: const Duration(seconds: 3),
-                  ),
-                  items: [
-                    "assets/img/b1.png",
-                    "assets/img/b2.png",
-                    "assets/img/b1.png",
-                  ].map((i) {
-                    return Builder(
-                      builder: (BuildContext context) {
-                        return Container(
-                          width: MediaQuery.of(context).size.width,
-                          margin: const EdgeInsets.symmetric(horizontal: 5.0),
-                          decoration: const BoxDecoration(
-                            color: Colors.transparent,
-                          ),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(10),
-                            child: Image.asset(
-                              i,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        );
-                      },
-                    );
-                  }).toList(),
-                ),
-                const SizedBox(
-                  height: 10.0,
-                ),
-                ServiceText(),
-                ServiceRow(),
-                ServiceRowTwo(),
-              ],
-            ),
-          ),
+        body: _buildBody(),
+      ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: const Color.fromARGB(255, 15, 75, 125),
+      title: Text('Welcome! $username'),
+      titleTextStyle: GoogleFonts.montserrat(
+        color: AppColors.whiteText,
+        fontSize: 15,
+        fontWeight: FontWeight.w600,
+      ),
+      elevation: 0,
+      leading: Builder(
+        builder: (BuildContext context) {
+          return IconButton(
+            icon: const Icon(Iconsax.menu, color: AppColors.whiteText),
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
+          );
+        },
+      ),
+      actions: _buildAppBarActions(),
+    );
+  }
+
+  List<Widget> _buildAppBarActions() {
+    return [
+      IconButton(
+        color: AppColors.whiteText,
+        icon: const Icon(Icons.notifications),
+        onPressed: () {
+          EasyLoading.showInfo("No new notifications.");
+        },
+      ),
+      IconButton(
+        color: AppColors.whiteText,
+        icon: const Icon(Icons.logout),
+        onPressed: _showLogoutDialog,
+      ),
+    ];
+  }
+
+  Widget _buildBody() {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: Column(
+          children: [
+            MyCarouselSlider(),
+            const SizedBox(height: 10.0),
+            ServiceText(),
+            ServiceRow(),
+            ServiceRowTwo(),
+          ],
         ),
       ),
     );
